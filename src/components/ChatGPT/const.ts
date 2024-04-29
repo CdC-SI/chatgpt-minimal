bc2fb69fc421b74c3503a17451d2017deee3fe46
kieran.schubert <schubert.kieran@gmail.com>
2024-04-29 13:14:54 +0200

    update const.ts with codespace test url

--- src/components/ChatGPT/const.ts
+++ src/components/ChatGPT/const.ts
@@ -1,2 +1,2 @@
 export const REACT_APP_QUERY_AUTOCOMPLETE_API_URL =
-  process.env.REACT_APP_QUERY_AUTOCOMPLETE_API_URL || 'http://localhost:8000';
+  process.env.REACT_APP_QUERY_AUTOCOMPLETE_API_URL || 'https://friendly-goggles-644jq4xv9942q-8000.app.github.dev/';
