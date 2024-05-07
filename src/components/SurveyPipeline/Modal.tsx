c69be36f1ba3b0c82c84f3b1b17a55b88cf49000
kieran.schubert <schubert.kieran@gmail.com>
2024-05-07 15:46:08 +0200

    update instructions

--- src/components/SurveyPipeline/Modal.tsx
+++ src/components/SurveyPipeline/Modal.tsx
@@ -33,7 +33,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
         <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', width: '80%', height: '80%' }}>
           {children}
-          <p>Bitte bearbeiten Sie die Frage und die Antwort, falls erforderlich.
+          <p>Bitte bearbeiten Sie die Frage, die Antwort, die Quelle (URL) und Ihren Namen, falls erforderlich.
           <br /><br />
               <ul>
                   <li>Klicken Sie auf <strong>Speichern</strong>, um die Änderungen in die Datenbank einzufügen.</li>
